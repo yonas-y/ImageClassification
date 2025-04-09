6bfe436440be1abf7a2cb1dd5a355630df3daed8
Yonas Y <yoni99y@gmail.com>
2025-04-09 14:50:46 +0300

    Updated API App!

--- api_app.py
+++ api_app.py
@@ -13,3 +13,10 @@ app = FastAPI()
 
 # Load model once at startup
 model = load_model("results/mnist_model.h5")
+
+
+def preprocess_image(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes)).convert("L").resize((28, 28))
+    img_array = np.array(image) / 255.0
+    img_array = img_array.reshape(1, 28, 28, 1)
+    return img_array
