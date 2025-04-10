72743c4343074c36aba91677d19ba64764b38676
Yonas Y <yoni99y@gmail.com>
2025-04-10 08:06:39 +0300

    Completed the API app script

--- api_app.py
+++ api_app.py
@@ -20,3 +20,16 @@ def preprocess_image(image_bytes):
     img_array = np.array(image) / 255.0
     img_array = img_array.reshape(1, 28, 28, 1)
     return img_array
+
+
+@app.post("/predict")
+async def predict(image_bytes: UploadFile = File(...)):
+    try:
+        image_bytes = await image_bytes.read()
+        img_array = preprocess_image(image_bytes)
+        prediction = model.predict(img_array)
+        prediction_digit = int(np.argmax(prediction))
+        return prediction_digit
+    except Exception as e:
+        return JSONResponse({"error": str(e)}, status_code=400)
+        
